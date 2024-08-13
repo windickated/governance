84f97b7c0c532df57ef90c4d478e8b9fc4e26fca
Dimon <notawid@gmail.com>
2024-08-13 12:22:12 +0200

    Added display with buttons

---
+++ src/components/display.svelte
@@ -0,0 +1,180 @@
+<script>
+const displayScreen = {
+  buttons: [
+    {
+      id: "switcher",
+      video: "/video.avif",
+      text: "/text.avif",
+      videoHover: "/video-hover.avif",
+      textHover: "/text-hover.avif"
+    },
+    {
+      id: "vote",
+      image: "/vote-clickable.avif",
+      hover: "/vote-hover.avif",
+      click: "/vote-active.avif",
+      inactive: "/vote-inert.avif"
+    }
+  ],
+  display: {
+    fullsize: "/display.avif",
+    mobilesize: "/displayMobile.avif",
+    BG: "/displayBG.avif",
+    mobileBG: "/displayMobileBG.avif"
+  }
+}
+
+// Format button
+let formatButtonState = false;
+let formatButtonHover = false;
+const switcherHandle = (event) => {
+  if (event.type === 'click') {
+    formatButtonState = !formatButtonState;
+  } else if (
+    event.type === 'mouseover' || event.type === 'mouseout') {
+    formatButtonHover = !formatButtonHover;
+  }
+}
+
+// Vote button
+let voteButtonState = true;
+let voteButtonHover = false;
+const voteHandle = (event) => {
+  if (voteButtonState === true) {
+    if (event.type === 'click') {
+      voteButtonState = !voteButtonState;
+    } else if (
+      event.type === 'mouseover' || event.type === 'mouseout') {
+        voteButtonHover = !voteButtonHover;
+    }
+  }
+}
+</script>
+
+
+<div class="display-screen">
+  <!-- svelte-ignore a11y-click-events-have-key-events a11y-no-noninteractive-element-interactions -->
+  <!-- svelte-ignore a11y-mouse-events-have-key-events -->
+  <div class="display-buttons">
+    <div
+      class="format"
+      role="button"
+      tabindex="0"
+      on:click={switcherHandle}
+      on:mouseover={switcherHandle}
+      on:mouseout={switcherHandle}
+    >
+      <img
+        class="format-btn {!formatButtonHover ? (formatButtonState ? 'visible' : '') : ''}"
+        src={displayScreen.buttons[0].video}
+        alt="Switcher"
+      />
+      <img
+        class="format-btn {!formatButtonHover ? (!formatButtonState ? 'visible' : '') : ''}"
+        src={displayScreen.buttons[0].text}
+        alt="Switcher"
+      />
+      <img
+        class="format-btn {!formatButtonState ? (formatButtonHover ? 'visible' : '') : ''}"
+        src={displayScreen.buttons[0].videoHover}
+        alt="Switcher"
+      />
+      <img
+        class="format-btn {formatButtonState ? (formatButtonHover ? 'visible' : '') : ''}"
+        src={displayScreen.buttons[0].textHover}
+        alt="Switcher"
+      />
+    </div>
+    <div
+      class="vote"
+      role="button"
+      tabindex="0"
+      on:click={voteHandle}
+      on:mouseover={voteHandle}
+      on:mouseout={voteHandle}
+    >
+      <img
+        class="vote-btn {!voteButtonHover ? (voteButtonState ? 'visible' : '') : ''}"
+        src={displayScreen.buttons[1].image}
+        alt="Vote"
+      />
+      <img
+        class="vote-btn {voteButtonState ? (voteButtonHover ? 'visible' : '') : ''}"
+        src={displayScreen.buttons[1].hover}
+        alt="Vote"
+      />
+      <img
+        class="vote-btn {!voteButtonState ? 'visible' : ''}"
+        src={displayScreen.buttons[1].click}
+        alt="Vote"
+      />
+      <img
+        class="vote-btn"
+        src={displayScreen.buttons[1].inactive}
+        alt="Vote"
+      />
+    </div>
+  </div>
+  <picture class="display">
+    <source srcset={displayScreen.display.mobilesize} media="(max-width: 600px)" />
+    <img src={displayScreen.display.fullsize} alt="Display" />
+  </picture>
+  <iframe id="video" title="YouTube" allowfullscreen></iframe>
+  <picture class="display-bg">
+    <source srcset={displayScreen.display.mobileBG} media="(max-width: 600px)" />
+    <img src={displayScreen.display.BG} alt="Background" />
+  </picture>
+</div>
+
+
+<style>
+  .display-screen {
+    position: relative;
+    width: 98vw;
+    margin-inline: 1vw;
+  }
+
+  .display, .display-bg {
+    height: auto;
+  }
+
+  .display-bg {
+    z-index: -10;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+
+  #video {
+    position: absolute;
+  }
+
+  .display-buttons {
+    z-index: 10;
+    position: absolute;
+    width: 27.5%;
+    bottom: 0;
+    right: 0;
+    background-image: url("/format.avif");
+    background-position: top;
+    background-size: contain;
+  }
+
+
+  .format-btn, .vote-btn {
+    display: none;
+    cursor: pointer;
+  }
+
+  .visible {
+    display: block;
+  }
+
+
+  @media screen and (max-width: 600px) {
+    .format-btn, .vote-btn {
+      display: none !important;
+    }
+  }
+
+</style>
